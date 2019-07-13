762a3d0edf7166c82119fc12c0f52cf6353b7c25
mites1997 <arnabfeb15@gmail.com>
2019-07-14 01:26:01 +0530

    task5

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,25 +1,24 @@
 package com.stackroute;
 
+import com.stackroute.demo.BeanLifeCycleDemoBean;
 import com.stackroute.domain.Config;
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+
 
 public class Main {
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(Config.class);
         context.scan("com.stackroute");
-        Movie mv=context.getBean("movie", Movie.class);
-        mv.printAboutMovieActor();
-        Movie mv1=context.getBean("movie",Movie.class);
-        System.out.println(mv==mv1);
+        //Movie mv=context.getBean("movie", Movie.class);
+        //mv.printAboutMovieActor();
+        //Movie mv1=context.getBean("movie",Movie.class);
+        // System.out.println(mv==mv1);
+      BeanLifeCycleDemoBean beanLifeCycleDemoBean=context.getBean(BeanLifeCycleDemoBean.class);
+        context.registerShutdownHook();
 
     }
 
--- src/main/java/com/stackroute/domain/Config.java
+++ src/main/java/com/stackroute/domain/Config.java
@@ -1,30 +1,46 @@
 package com.stackroute.domain;
 
+import com.stackroute.demo.BeanLifeCycleDemoBean;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-
+//@ComponentScan(basePackages = "com.stackroute")
 public class Config {
 
     @Bean
     public Actor actor()
     {
-        return new Actor("Tom","male",42);
+        return new Actor("Sabyasachi","male",22);
     }
 
     @Bean
     public Actor actor1()
     {
-        return new Actor("John","male",32);
+        return new Actor("Mites","male",22);
     }
 
     @Bean
     public Actor actor2() {
-        return new Actor("Ron", "male", 22);
+        return new Actor("Haria", "male", 22);
     }
 
+    /* @Bean({"movie1","movie2"})
+     @Scope("prototype")
+     public Movie makeMovieObject()
+     {
+         Movie movie =new Movie();
+         return movie;
+     }*/
+    @Bean(initMethod = "customInit",destroyMethod = "customDestroy")
+    public BeanLifeCycleDemoBean beanLifeCycleDemoBean()
+    {
+        BeanLifeCycleDemoBean beanLifecycleDemoBean=new BeanLifeCycleDemoBean();
+        return beanLifecycleDemoBean;
+
+    }
 
 }
